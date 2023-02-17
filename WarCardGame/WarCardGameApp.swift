e0d08bdf421e522c405f928376948c9d3aeb500e
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-17 08:19:02 -0600

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Ashlyn Barrera on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
